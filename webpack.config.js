5efa259e17035620bcf2ecb2ecb77de3c2780a80
James Amaya <jameseamaya@gmail.com>
2017-05-08 13:39:52 -0400

    Add start and pause

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+// const path = require('path')
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./lib/tetris.js",
+//   output: {
+//     path: path.resolve(__dirname),
+//     filename: "bundle.js"
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: [".js", ".jsx", "*" ]
+//   },
+//   devtool: 'source-map',
+// };
+
 const path = require('path')
 
 module.exports = {
